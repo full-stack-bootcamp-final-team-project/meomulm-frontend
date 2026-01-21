203fa7f3aa9873a84804429043095f3e601fad13
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2026-01-21 16:06:06 +0900

    chore/search-bar-setting

--- lib/features/map/presentation/screens/map_result_screen.dart
+++ lib/features/map/presentation/screens/map_result_screen.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:meomulm_frontend/core/utils/date_people_utils.dart';
+import 'package:meomulm_frontend/core/widgets/appbar/search_bar_widget.dart';
 
+/*
+지도 검색 결과 스크린
+ */
 class MapResultScreen extends StatelessWidget {
   const MapResultScreen({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('테스트'),
-        leading: IconButton(
-          onPressed: () => context.go('/'),
-          icon: Icon(Icons.arrow_back),
-        ),
-      ),
+      appBar:SearchBarAppBar(
+
+        dateText: DatePeopleTextUtil.todayToTomorrow(),
+        peopleCount: 2,
+        onFilter: () {},
+      )
+      ,
       body: SafeArea(
         child: Stack(
           children: [
@@ -29,136 +32,6 @@ class MapResultScreen extends StatelessWidget {
               ),
             ),
 
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: Material(
-                color: Colors.transparent,
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
-                  ),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 18),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
-                      ),
-                      border: Border(
-                        bottom: BorderSide(color: Colors.black12, width: 1.5),
-                      ),
-                    ),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8, left: 3),
-                          child: Row(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.arrow_back),
-                              ),
-
-                              Expanded(
-                                child: Container(
-                                  height: 44,
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 12,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    border: Border.all(color: Colors.black38),
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      const Expanded(
-                                        child: TextField(
-                                          decoration: InputDecoration(
-                                            border: InputBorder.none,
-                                            isDense: true,
-                                            hint: Text('종로',style: TextStyle(fontWeight: FontWeight.bold),),
-                                          ),
-                                          enabled: false,),
-                                      ),
-
-                                      IconButton(
-                                        padding: EdgeInsets.zero,
-                                        constraints: const BoxConstraints(),
-                                        icon: const Icon(
-                                          Icons.cancel,
-                                          size: 20,
-                                          color: Colors.grey,
-                                        ),
-                                        onPressed: () {},
-                                      ),
-
-                                      const VerticalDivider(
-                                        thickness: 1,
-                                        width: 20,
-                                      ),
-
-                                      Column(
-                                        mainAxisAlignment:
-                                        MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                        children: const [
-                                          Text(
-                                            '12.22 - 12.23',
-                                            style: TextStyle(
-                                              fontSize: 12,
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                          Text(
-                                            '인원 2',
-                                            style: TextStyle(
-                                              fontSize: 11,
-                                              color: Colors.black54,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-
-                              const SizedBox(width: 8),
-
-                              InkWell(
-                                borderRadius: BorderRadius.circular(18),
-                                onTap: () {},
-                                child: Container(
-                                  width: 36,
-                                  height: 36,
-                                  decoration: BoxDecoration(
-                                    border: Border.all(color: Colors.blue),
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: IconButton(
-                                    icon: const Icon(
-                                      Icons.tune,
-                                      color: Colors.blue,
-                                      size: 18,
-                                    ),
-                                    onPressed: () {},
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
 
             Positioned(
               left: 16,
